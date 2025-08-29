07843b577786775143fb8507a1554a61e6f93ca2
katwiecz <katawiecz@gmail.com>
2025-08-29 15:07:36 +0200

    Zmień CORS z * na whitelistę Twoich domen (np. https://kasiaaichatbot.me i poddomeny).

--- api/chat.js
+++ api/chat.js
@@ -28,7 +28,7 @@ const path = require("path");
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 module.exports = async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://kasiaaichatbot.me");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
